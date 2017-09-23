0c6410556bf7293d5518b1c93c5f81be8c0b86e4
Benzamin Yi <yib544@newschool.edu>
2017-09-23 21:58:33 +0000

    Updated Week 02

--- Week02/week02v2.js
+++ Week02/week02v2.js
@@ -3,18 +3,15 @@ var cheerio = require('cheerio');
 var content = fs.readFileSync('data/m04.txt');
 var $ = cheerio.load(content);
 //starter code
-
+var final = [];
 //first, find the biggest russian doll - tbody. then move in.
  $('tbody').find('tr').each(function(i, elem) {
      //use $(elem) to get in the next level.
     $(elem).find('td').eq(0).each(function(i,elem) {
-        //pick first td of the three only. the others have extra text.
         var address = $(elem).contents().slice(3).eq(3).text().trim();
-        //create variable to output the result. so you call the elem, get its contents, slice (choose the element line you want so it'll take the next <x>.)
-        //.eq(3) is the code that reduces the result into just that line, otherwise you get all this other text. it basically executes the slice before it.
-        //text to output text, and trim the output afterwards
-//print
-console.log(address);
+//print to array
+    final.push(address + ' New York, NY')
     });
 });
     
+    fs.writeFileSync('./data/output2.json',JSON.stringify(final))
