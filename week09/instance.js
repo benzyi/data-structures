38d5f2ec2980723d0b36482a3fae5f6c8f8b2453
Benzamin Yi <yib544@newschool.edu>
2017-11-02 20:55:30 +0000

    Week 10

--- week09/instance.js
+++ week09/instance.js
@@ -17,7 +17,10 @@ app.get('/', function(req, res) {
     // SQL query
     var q = `SELECT EXTRACT(DAY FROM sensortime AT TIME ZONE 'America/New_York') as sensorday, 
              EXTRACT(MONTH FROM sensortime AT TIME ZONE 'America/New_York') as sensormonth, 
-             count(*) as num_obs
+             count(*) as num_obs,
+             count(ir) [ FILTER ( WHERE true ) ] as tv_on,
+             count(tilt) [ FILTER ( WHERE false ) ] as futon_down,
+             count(tilt) [ FILTER ( WHERE true ) ] as futon_up
              FROM irTilt 
              GROUP BY sensormonth, sensorday;`;
              
