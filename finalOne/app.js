d84a84313c3a4d8d832d505a538d5e0eb5265b15
Benzamin Yi <yib544@newschool.edu>
2017-11-27 20:30:41 +0000

    Full App

--- finalOne/app.js
+++ finalOne/app.js
@@ -13,12 +13,12 @@ db_credentials.port = 5432;
 
 // Mongo
 var collName = 'meetings';
-// var MongoClient = require('mongodb').MongoClient;
-// var url = process.env.ATLAS;
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.ATLAS;
 
 // HTML wrappers for AA data
-// var index1 = fs.readFileSync("index1.txt");
-// var index3 = fs.readFileSync("index3.txt");
+var index1 = fs.readFileSync("index1.txt");
+var index3 = fs.readFileSync("index3.txt");
 
 app.get('/', function(req, res) {
     // Connect to the AWS RDS Postgres database
@@ -41,78 +41,78 @@ app.get('/', function(req, res) {
     client.end();
 });
 
-// app.get('/aa', function(req, res) {
+app.get('/aa', function(req, res) {
 
-//     MongoClient.connect(url, function(err, db) {
-//         if (err) {return console.dir(err);}
+    MongoClient.connect(url, function(err, db) {
+        if (err) {return console.dir(err);}
         
-//         var dateTimeNow = new Date();
-//         var today = dateTimeNow.getDay();
-//         var tomorrow;
+        var dateTimeNow = new Date();
+        var today = dateTimeNow.getDay();
+        var tomorrow;
         
-//         if (today == 6) {today = 0;}
+        if (today == 6) {today = 0;}
         
-//         // if (today == 7) {today = 0;} //need to change my data so sunday is 0 instead of 7.. or this
-//         // else if (today == 6) {tomorrow = 0;} 
+        // if (today == 7) {today = 0;} //need to change my data so sunday is 0 instead of 7.. or this
+        // else if (today == 6) {tomorrow = 0;} 
         
-//         else {tomorrow = today + 1}
-//         var hour = dateTimeNow.getHours();
+        else {tomorrow = today + 1}
+        var hour = dateTimeNow.getHours();
 
-//         var collection = db.collection(collName);
+        var collection = db.collection(collName);
     
-//         collection.aggregate([ // start of aggregation pipeline
-//             // match by day and time
-//             { $unwind : "$day"  },
-//             { $match : 
-//                 { $or : [
-//                     { $and: [
-//                         { "day.day" : 2 } , { "day.startHour" : { $gte: 19 } }
-//                     ]},
-//                     { $and: [
-//                         { "day.day" : 3 } , { "day.startHour" : { $lte: 4 } }
-//                     ]}
-//                 ]}
-//             },
-//             { $unwind : "$latLong" },
-//             // group by meeting group
-//             { $group : { _id : {
-//                 latLong : "$latLong",
-//                 lat : "$latLong.lat",
-//                 long : "$latLong.long",
-//                 meetingName : "$program",
-//                 meetingAddress1 : "$mainAddress",
-//                 meetingAddress2 : "$secondAddress",
-//                 meetingDetails : "$notes",
-//                 meetingWheelchair : "$wheelchair",
-//                 },
-//                     meetingDay : { $push : "$day.day" },
-//                     meetingStartTime : { $push : "$day.startTime" }, 
-//                     meetingType : { $push : "$day.type" }
-//             }
-//             },
+        collection.aggregate([ // start of aggregation pipeline
+            // match by day and time
+            { $unwind : "$day"  },
+            { $match : 
+                { $or : [
+                    { $and: [
+                        { "day.day" : 2 } , { "day.startHour" : { $gte: 19 } }
+                    ]},
+                    { $and: [
+                        { "day.day" : 3 } , { "day.startHour" : { $lte: 4 } }
+                    ]}
+                ]}
+            },
+            { $unwind : "$latLong" },
+            // group by meeting group
+            { $group : { _id : {
+                latLong : "$latLong",
+                lat : "$latLong.lat",
+                long : "$latLong.long",
+                meetingName : "$program",
+                meetingAddress1 : "$mainAddress",
+                meetingAddress2 : "$secondAddress",
+                meetingDetails : "$notes",
+                meetingWheelchair : "$wheelchair",
+                },
+                    meetingDay : { $push : "$day.day" },
+                    meetingStartTime : { $push : "$day.startTime" }, 
+                    meetingType : { $push : "$day.type" }
+            }
+            },
             
-//             // group meeting groups by latLong
-//             {
-//                 $group : { _id : { 
-//                     latLong : "$_id.latLong"},
-//                     meetingGroups : { $push : {groupInfo : "$_id", meetingDay : "$meetingDay", meetingStartTime : "$meetingStartTime", meetingType : "$meetingType" }}
-//                 }
-//             }
+            // group meeting groups by latLong
+            {
+                $group : { _id : { 
+                    latLong : "$_id.latLong"},
+                    meetingGroups : { $push : {groupInfo : "$_id", meetingDay : "$meetingDay", meetingStartTime : "$meetingStartTime", meetingType : "$meetingType" }}
+                }
+            }
         
-//             ]).toArray(function(err, docs) { // end of aggregation pipeline
-//             if (err) {console.log(err)}
+            ]).toArray(function(err, docs) { // end of aggregation pipeline
+            if (err) {console.log(err)}
             
-//             else {
-//                 res.writeHead(200, {'content-type': 'text/html'});
-//                 res.write(index1);
-//                 res.write(JSON.stringify(docs));
-//                 res.end(index3);
-//             }
-//             db.close();
-//         });
-//     });
+            else {
+                res.writeHead(200, {'content-type': 'text/html'});
+                res.write(index1);
+                res.write(JSON.stringify(docs));
+                res.end(index3);
+            }
+            db.close();
+        });
+    });
     
-// });
+});
 
 // app.listen(process.env.PORT, function() {
 app.listen(3000, function() {
