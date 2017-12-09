92eba200710c3695b249560402e08bd728dd53ed
Benzamin Yi <yib544@newschool.edu>
2017-12-09 05:30:32 +0000

    New SQL

--- finalOne/app.js
+++ finalOne/app.js
@@ -36,7 +36,7 @@ app.get('/', function(req, res) {
              bool_or(ir) as tv_turnedOn,
              bool_or(tilt) as couch_mode
              FROM irTilt 
-             GROUP BY sensormonth, sensorday, sensorhour;`;
+             GROUP BY sensormonth, sensorday, sensorhour, sensorminute;`;
              
     client.connect();
     client.query(q, (qerr, qres) => {
